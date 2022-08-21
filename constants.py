d70b610d60ce6e91b23e58ae5fce7e765182c4fd
Aldo Escobar <aoelvp@Aldos-MacBook-Pro.local>
2022-08-21 03:17:46 -0300

    miau

---
+++ constants.py
@@ -0,0 +1,6 @@
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 768
+FPS = 30
+SAD_PEDRO_SHEET = './assets/images/sadpedro.bmp'
+SPRITE_SHEET = './assets/images/Insectoids.bmp'
+SPRITE_SHEET_EXPLOSION = './assets/images/Explosion.png'
